20f4e84394e03aa03a0166d9f5ddc61a14bd9531
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2023-12-08 20:38:26 +0700

    Add files via upload

--- DeAn/src/NhanVien/DanhSachNhanVien.java
+++ DeAn/src/NhanVien/DanhSachNhanVien.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 
 final class DanhSachNhanVien {
 	private NhanVien dsNhanVien[];
+	private NhanVien nv = new NhanVien();
 	private int tongNV; 
 	private int soNVHH; // so nhan vien hien huu ( bien dem cho so nv kh nam trong danh sach xoa ) 
 	File d = new File("src/InputOutput/DanhSachNhanVien.txt");
@@ -19,6 +20,8 @@ final class DanhSachNhanVien {
 	//constructor
 	public DanhSachNhanVien(int n) {
 		dsNhanVien = new NhanVien[n];
+		this.tongNV=0;
+		this.soNVHH=0;
 	}
 	public DanhSachNhanVien(NhanVien dsNhanVien[], int tongNV, int soNVHH) {
 		this.dsNhanVien = dsNhanVien;
@@ -30,7 +33,6 @@ final class DanhSachNhanVien {
 		this.tongNV = a.tongNV;
 		this.soNVHH = a.soNVHH;
 	}
-	
 	
 	// get/set
 	public NhanVien[] getDsNhanVien() {
@@ -56,32 +58,18 @@ final class DanhSachNhanVien {
 	Scanner sc = new Scanner(System.in);
 	public void them() {
 		System.out.println(" ------------ Them nhan vien ------------");
-		System.out.print(" - Ban muon them bao nhieu nhan vien: ");
-		int n = sc.nextInt(); int m = 0;
-		if(n <= 0) {
-			System.out.println("So nhan vien khong hop le !!!"); 
-		}
-		for(int i=0;i<n;i++) {
-			this.tongNV++; this.soNVHH++; m++; 
-			System.out.println(" 	$ Nhap thong tin nhan vien thu " + this.tongNV);
-			dsNhanVien = Arrays.copyOf(dsNhanVien, this.tongNV);
-			dsNhanVien[this.tongNV - 1] = new NhanVien();
-			dsNhanVien[this.tongNV - 1].nhap(); int k = this.tongNV - 1;
-//			try {
-//				if(!d.exists()) {
-//					d.createNewFile();
-//				} 
-//				FileWriter f = new FileWriter(d,true);
-//				BufferedWriter bf = new BufferedWriter(f);
-//				bf.write(dsNhanVien[k].getHo() + "/" + dsNhanVien[k].getTen() + "/" + dsNhanVien[k].getMaNhanVien() + '/' + dsNhanVien[k].getSdt());
-//				
-//			} catch (IOException ioe) {
-//				System.out.println("Erorr!!!");
-//			}
-		}
+		this.tongNV++; this.soNVHH++; 
+		System.out.println(" 	$ Nhap thong tin nhan vien $ ");
+		dsNhanVien = Arrays.copyOf(dsNhanVien, this.tongNV);
+		dsNhanVien[this.tongNV - 1] = new NhanVien();
+		dsNhanVien[this.tongNV - 1].nhap(); 
 		System.out.println("");
-		if( m == 0) {
-			System.out.println(" --- Hoan tat thao tac them nhan vien ---");
+		System.out.print(" Tiep tuc (1) | Dung lai (phim bat ky): ");
+		int n = sc.nextInt();
+		System.out.println("");
+		if(n == 1) { them(); } 
+		else { 
+			System.out.println(" -------- Hoan tat thao tac --------");
 		}
 	}	
 	public void docfile() {
@@ -89,19 +77,21 @@ final class DanhSachNhanVien {
 			if(!d.exists()) {
 				d.createNewFile();
 			}
-			int n = 0;
 			FileReader fr = new FileReader(d);
 			BufferedReader br = new BufferedReader(fr);
 			String lines = br.readLine();
 			while(lines != null) {
 				String []line = lines.split("/");
-				dsNhanVien[n] = new NhanVien(line[0], line[1], line[2], line[3], Integer.parseInt(line[4]));
-				n++;
+				dsNhanVien[tongNV] = new NhanVien(line[0], line[1], line[2], line[3], Integer.parseInt(line[4]));
+				if(line[4].equalsIgnoreCase("0")) {
+					this.soNVHH++;
+				}
+				tongNV++;
 				lines = br.readLine();
 			}
 			br.close();
 		} catch(IOException ioe) {
-			System.out.println("error!");
+			System.out.println("File Error!");
 		}
 		
 	}
@@ -110,141 +100,128 @@ final class DanhSachNhanVien {
 			if(!d.exists()) {
 				d.createNewFile();
 			}
+			FileWriter er = new FileWriter(d);
+			BufferedWriter cr = new BufferedWriter(er);
+			cr.write("");
+			cr.close();
 			FileWriter fr = new FileWriter(d, true);
 			BufferedWriter br = new BufferedWriter(fr);
 			for(NhanVien nv : dsNhanVien) {
 				if(nv != null) {
-					br.write(nv.getHo() + "/" + nv.getTen() + "/" + nv.getMaNhanVien() + "/" + nv.getSdt() + "/" + nv.getDeleted() + "\n");
+					br.write(nv.getMaNhanVien() + "/" + nv.getHo() + "/" + nv.getTen() + "/" + nv.getSdt() + "/" + nv.getDeleted() + "\n");
 				} else {
 					break;
 				}
 			}
 			br.close();
 		} catch(IOException ioe) {
-			System.out.println("error!");
+			System.out.println("File Error!");
 		}
 	}
 	public void xemds() {
-//		docfile();
-        //xuat du
-//        System.out.printf("%-15s %-5s %-10s %-15s","Ma nhan vien","Ho","Ten","SoDienThoai");
-//        System.out.println("");
-//        for(int i=0;i<this.tongNV;i++){
-//            dsNhanVien[i].xuat(); System.out.println("");
-//        }
 		System.out.println(" ------------ Danh sach nhan vien ------------");
 		System.out.printf("%-15s %-7s %-15s %-10s","Ma nhan vien","Ho","Ten","SoDienThoai");
 		System.out.println();
+		int checkList = -1;
         for(NhanVien nv : dsNhanVien) {
-        	if (nv.getDeleted() != 1) {
-            	nv.xuat(); System.out.println("");
+        	if (nv != null && nv.getDeleted() != 1) {
+            	nv.xuat(); System.out.println(""); checkList++;
         	}
         }
+        if(checkList == -1) {
+            System.out.println("");
+        	System.out.println(" -------- Danh sach trong --------");
+        }
+	}
+	public void xemdsXoa() {
+		System.out.println(" ------------ Danh sach nhan vien da xoa ------------");
+		System.out.printf("%-15s %-7s %-15s %-10s","Ma nhan vien","Ho","Ten","SoDienThoai");
+		System.out.println();
+		int checkList = -1;
+		for(NhanVien nv : dsNhanVien) {
+			if(nv != null && nv.getDeleted() == 1) {
+				nv.xuat(); System.out.println(""); checkList++;
+			}
+		}
+		System.out.println("");
+		if(checkList != -1) {
+			System.out.print(" Xoa vinh vien(1) | Khoi phuc(2) | Thoat(0): ");
+	        int n = sc.nextInt();
+	        System.out.println("");
+	        switch (n) {
+	        	case 1:
+	        		System.out.println(" ------------ Xoa vinh vien ------------");
+	        		sc.nextLine();
+	        		System.out.print(" - Nhap ma nhan vien can xoa: ");
+	        		String maXoa = sc.nextLine();
+	        		int delete_point = -1;
+	        		for(int i = 0;i < this.tongNV;i++) {
+	        			if(dsNhanVien[i] != null && dsNhanVien[i].getDeleted() == 1 && dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(maXoa)) {
+	        				delete_point = i;
+	        				break;
+	        			}
+	        		}
+	        		if(delete_point != -1) {
+	        			for(int i=delete_point;i<this.tongNV - 1;i++) {
+	        				dsNhanVien[i] = dsNhanVien[i+1];
+	        			}
+	        			this.tongNV--;
+	        			dsNhanVien = Arrays.copyOf(dsNhanVien, this.tongNV);
+	        			System.out.println("");
+	        			System.out.println(" -------- Hoan tat thao tac --------");
+	        		} else { 
+	        			System.out.println("");
+	        			System.out.println(" -------- Khong tim thay ma nhan vien --------");
+	        		} 
+	        		break;
+	        	case 2:
+	        		System.out.println(" ------------ Khoi phuc ------------");
+	        		sc.nextLine();
+	        		System.out.print(" - Nhap ma nhan vien can khoi phuc: ");
+	        		String maKP = sc.nextLine(); int m = 0;
+	        		for(NhanVien nv : dsNhanVien) {
+	        			if(nv != null && nv.getDeleted() == 1 && nv.getMaNhanVien().equalsIgnoreCase(maKP)) {
+	        				nv.setDeleted(0); m++; this.soNVHH++;
+	        				System.out.println("");
+	        				System.out.println(" -------- Hoan tat thao tac --------");
+	        				break;
+	        			}
+	        		}
+	        		if(m != 1) {
+	        			System.out.println("");
+	        			System.out.println(" -------- Khong tim thay ma nhan vien --------");
+	        		}
+	        	case 0:
+	        		break;
+	        	default:
+	        		System.out.println(" Lua chon khong hop le, xin thu lai !!!");
+	        		xemdsXoa();
+	        		break;
+	        }
+		} else {
+			System.out.println(" -------- Danh sach trong --------");
+		}
+        
 	}
 	public void xoa() {
 		System.out.println(" ------------ Xoa nhan vien ------------");
-		System.out.println("	1. Xoa nhan vien.");
-		System.out.println("	2. Xem danh sach nhan vien da xoa.");
-		System.out.println("	3. Thoat.");
-		System.out.print(" - Chon hanh dong: ");
-		int n = sc.nextInt();
-		System.out.println();
-		switch (n) {
-			case 1:
-				System.out.println(" ----------------------------------");
-				System.out.print(" - Hay nhap ma nhan vien can xoa: ");
-				String x = sc.next(); int m = 1;
-				for(int i=0;i<this.tongNV;i++) {
-					if(dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(x) && dsNhanVien[i].getDeleted() != 1) {
-						dsNhanVien[i].setDeleted(1); m++; this.soNVHH--;
-					}
-				}
+		System.out.print(" - Hay nhap ma nhan vien can xoa: ");
+		String x = sc.next(); int m = 0;
+		for(int i=0;i<this.tongNV;i++) {
+			if(dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(x) && dsNhanVien[i].getDeleted() != 1) {
+				dsNhanVien[i].setDeleted(1); m++; this.soNVHH--;
 				System.out.println("");
-				if(m == 1) {
-					System.out.println(" ----- Khong tim thay nhan vien ------");
-				} else {
-					System.out.println(" ------ Hoan tat thao tac xoa nhan vien ------");
-				}
-				break;
-			case 2:
-				System.out.println(" ------- Danh sach nhan vien da xoa --------");
-				System.out.printf("%-15s %-7s %-15s %-10s","Ma nhan vien","Ho","Ten","SoDienThoai");
-				System.out.println("");
-				for(NhanVien nv : dsNhanVien) {
-			        	if (nv.getDeleted() == 1) {
-			            	nv.xuat(); System.out.println("");
-			        	}
-			    }
-				System.out.println("");
-				 System.out.println("        1. Khoi phuc nhan vien.");
-				 System.out.println("        2. Xoa vinh vien.");
-				 System.out.println("        3. Thoat.");
-				 System.out.print(" - Chon hanh dong: ");
-				 int a = sc.nextInt();
-				 System.out.println("");
-				 switch (a) {
-				 	case 1:
-				 		System.out.println(" ----------- Khoi phuc ------------");
-				 		System.out.print(" - Hay nhap ma nhan vien can khoi phuc: ");
-				 		String maKP = sc.next(); int k = 1;
-				 		for(int i=0;i<this.tongNV;i++) {
-				 			if(dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(maKP) && dsNhanVien[i].getDeleted() == 1) {
-				 				dsNhanVien[i].setDeleted(0); this.soNVHH++;
-				 				k++; break;
-				 			}
-				 		}
-				 		System.out.println("");
-				 		if(k == 1) {
-				 			System.out.println(" ------ Khong tim thay ma nhan vien ------");
-				 		} else {
-				 			System.out.println(" ------ Hoan tat khoi phuc nhan vien ------");
-				 		}
-				 		break;
-				 	case 2: 
-				 		System.out.println(" ---------- Xoa vinh vien ----------");
-				 		System.out.print(" - Hay nhap ma nhan vien can xoa: ");
-				 		String xoaVV = sc.next();
-				 		int delete_point = -1;
-						for(int i=0;i<this.tongNV;i++) {
-							if(dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(xoaVV) && dsNhanVien[i].getDeleted() == 1) {
-								delete_point = i;
-								break;
-							}
-						}
-						if(delete_point != -1) {
-							for(int i=delete_point;i<this.tongNV-1;i++) {
-								dsNhanVien[i] = dsNhanVien[i+1];
-							} 
-							this.tongNV--;
-							dsNhanVien = Arrays.copyOf(dsNhanVien, this.tongNV);
-							System.out.println("");
-							System.out.println(" ------ Hoan tat thao tac xoa vinh vien ------");
-						} else {
-							System.out.println("");
-							System.out.println(" ------ Khong tim thay ma nhan vien ------");
-						} 
-						break;
-				 	case 3:
-				 		break;
-				 	default: 
-				 		System.out.println(" Lua chon khong hop le, xin thu lai !!!");
-				 		break;
-				 }
-				 break;
-			case 3:
-				break;
-			default: 
-				System.out.println(" Lua chon khong hop le, xin vua long chon lai !!!");
-				xoa(); break;
+				System.out.println(" -------- Hoan tat thao tac --------");
+			}
+		}
+		if(m != 1) {
+			System.out.println("");
+			System.out.println(" -------- Khong tim thay ma nhan vien --------");
 		}
 	}
 	public void timkiem() {
 		System.out.println(" ------------ Tim kiem nhan vien ------------");
-		System.out.println("	1. Tim kiem theo ma nhan vien."); // khoang cach vao = 1 tab
-		System.out.println("	2. Tim kiem theo ho ten nhan vien."); // ma va sdt deu la khoa chinh nen kh the tim kiem chung.
-		System.out.println("	3. Tim kiem theo so dien thoai nhan vien.");
-		System.out.println("	4. Thoat.");
-		System.out.print(" - Chon hanh dong: ");
+		System.out.print(" Ma nhan vien(1) | Ho ten(2) | So dien thoai(3) | Thoat(0): ");
 		int n = sc.nextInt(); int m = 1;
 		switch (n) {
 			case 1: 
@@ -286,7 +263,7 @@ final class DanhSachNhanVien {
 					}
 				}
 				break;
-			case 4: 
+			case 0: 
 				m = -1;
 				break;
 			default:
@@ -308,41 +285,36 @@ final class DanhSachNhanVien {
 		System.out.println(" ------------ Sua nhan vien ------------");
 		System.out.print(" - Hay nhap ma nhan vien can sua: ");
 		String x = sc.next(); int m = 1;
+		System.out.println("");
 		// Nhap ma nhan vien sau do tuy chon cach sua.
 		for (int i=0;i<this.tongNV;i++) {
 			if(dsNhanVien[i].getMaNhanVien().equalsIgnoreCase(x) && dsNhanVien[i].getDeleted() != 1) {
-				m++;
-				System.out.println("	1. Sua ma nhan vien.");
-				System.out.println("	2. Sua ho ten nhan vien.");
-				System.out.println("	3. Sua so dien thoai.");
-				System.out.println("	4. Sua tat ca thong tin.");
-				System.out.println(" 	5. Thoat.");
-				System.out.print(" - Chon hanh dong: ");
-				int n = sc.nextInt();
+				System.out.print(" Ma nhan vien(1) | Ho ten(2) | So dien thoai(3) | Tat ca(4) | Thoat(0): ");
+				int n = sc.nextInt(); m++;
 				switch (n) {
 					case 1:
-						System.out.print(" - Nhap ma nhan vien: ");
+						System.out.print(" - Sua ma nhan vien: ");
 						String ma = sc.next();
 						dsNhanVien[i].setMaNhanVien(ma);
 						break;
 					case 2:
-						System.out.print(" - Nhap ho nhan vien: ");
+						System.out.print(" - Sua ho nhan vien: ");
 						String ho = sc.next();
 						sc.nextLine();
-						System.out.print(" - Nhap ten nhan vien: ");
+						System.out.print(" - Sua ten nhan vien: ");
 						String ten = sc.nextLine();
 						dsNhanVien[i].setHo(ho);
 						dsNhanVien[i].setTen(ten);
 						break;
 					case 3: 
-						System.out.print(" - Nhap sdt nhan vien: ");
+						System.out.print(" - Sua sdt nhan vien: ");
 						String sdt = sc.next();
 						dsNhanVien[i].setSdt(sdt);
 						break;
 					case 4: 
 						dsNhanVien[i].nhap();
 						break;
-					case 5:
+					case 0:
 						m = -1;
 						break;
 					default:
@@ -360,7 +332,7 @@ final class DanhSachNhanVien {
 			;
 		} else {
 			System.out.println("");
-			System.out.println(" ----- Hoan tat sua thong tin nhan vien -----");
+			System.out.println(" ----- Hoan tat thao tac -----");
 		}
 	}
 	public void thongke() {
--- DeAn/src/NhanVien/QuanLyDSNV.java
+++ DeAn/src/NhanVien/QuanLyDSNV.java
@@ -25,56 +25,83 @@ public class QuanLyDSNV {
 	}
 	
 	// methods
+	public void clearScreen(int n) {
+		for(int i=0;i<n;i++) {
+			System.out.println();
+		}
+	}
 	public void menu() {
 		ds.docfile();
-		System.out.println();
-		System.out.println("   =========================================");
-		System.out.println("   !           ----- Menu -----            !");
-		System.out.println("   !        1. Them nhan vien.             !");
-		System.out.println("   !        2. Xem danh sach nhan vien.    !");
-		System.out.println("   !        3. Tim kiem nhan vien.         !");
-		System.out.println("   !        4. Sua nhan vien.              !");
-		System.out.println("   !        5. Xoa nhan vien.              !");
-		System.out.println("   !        6. Thong ke.                   !");
-		System.out.println("   !        7. Thoat.                      !");
-		System.out.println("   =========================================");
-		System.out.println("");
-		Scanner sc = new Scanner(System.in);
-		System.out.print(" - Chon hanh dong: ");
-		int n = sc.nextInt();
-		System.out.println("");
-		switch(n) {
-			case 1:
-				ds.them();
-				menu();
-				break;
-			case 2:
-				ds.xemds();
-				menu();
-				break;
-			case 3:
-				ds.timkiem();
-				menu();
-				break;
-			case 4:
-				ds.sua();
-				menu();
-				break;
-			case 5:
-				ds.xoa();
-				menu();
-				break;
-			case 6:
-				ds.thongke();
-				break;
-			case 7:
-				System.out.println("Hen gap lai !!!");
-				ds.ghifile();
-				break;
-			default: 
-				System.out.println(" Lua chon kh hop le. Hay thu lai!!!");
-				menu();
-                break;
+		//8. Xem ds da xoa => Xoa ; Khoi phuc
+		//5. Xoa => Xoa ; Xoa vinh vien
+		while(true) {
+			System.out.println("");
+			System.out.println("   =========================================");
+			System.out.println("   !           ----- Nhan Vien -----       !");
+			System.out.println("   !        1. Them nhan vien.             !");
+			System.out.println("   !        2. Danh sach nhan vien.        !");
+			System.out.println("   !        3. Danh sach nhan vien da xoa. !");
+			System.out.println("   !        4. Tim kiem nhan vien.         !");
+			System.out.println("   !        5. Sua nhan vien.              !");
+			System.out.println("   !        6. Xoa nhan vien.              !");
+			System.out.println("   !        7. Thong ke.                   !");
+			System.out.println("   !        0. Thoat.                      !");
+			System.out.println("   =========================================");
+			System.out.println("");
+			Scanner sc = new Scanner(System.in);
+			System.out.print(" - Chon hanh dong: ");
+			int n = sc.nextInt();
+			System.out.println("");
+			switch(n) {
+				case 1:
+					clearScreen(5);
+					ds.them();
+					clearScreen(5);
+					break;
+				case 2:
+					clearScreen(5);
+					ds.xemds();
+					System.out.println("");
+					// Thực hiện thao tác nhấn bất kì để tắt danh sách NV.
+					sc.nextLine();
+					System.out.print(" Thoat (phim bat ky): ");
+					String checkOut = sc.nextLine();
+					clearScreen(5);
+					break;
+				case 3:
+					clearScreen(5);
+					ds.xemdsXoa();
+					clearScreen(5);
+					break;
+				case 4: // Vinh lam tim kiem
+					clearScreen(5);
+					ds.timkiem();
+					clearScreen(5);
+					break;
+				case 5:
+					clearScreen(5);
+					ds.sua();
+					clearScreen(5);
+					break;
+				case 6:
+					clearScreen(5);
+					ds.xoa();
+					clearScreen(5);
+					break;
+				case 7:
+					clearScreen(5);
+					ds.thongke();
+					clearScreen(5);
+					break;
+				case 0:
+					System.out.println("Hen gap lai !!!");
+					ds.ghifile();
+					return;
+				default: 
+					System.out.println(" Lua chon kh hop le. Hay thu lai!!!");
+					menu();
+	                break;
+			}
 		}
 	}
 }
