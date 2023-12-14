0fccad5d886c49e4636130fa55c23410419424a6
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2023-12-14 13:49:28 +0700

    Add files via upload

--- DeAn/src/Main/Main.java
+++ DeAn/src/Main/Main.java
@@ -6,17 +6,32 @@ import SanBay.QuanLyDSSB;
 import HoaDon.QuanLyDSHD;
 import ChuyenBay.QuanLyDSCB;
 import TEST.DanhSachVe;
-
+import HoaDon.QuanLyDSHD;
+import ChiTietHoaDon.QuanLyDSCTHD;
+import MayBay.QuanLyDSMB;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[]args) {
 		while(true) {
-			System.out.print(" 1. NV | 2. KH | 3. SB | 4.HD | 5.CB | 6.Ve : ");
+			System.out.println(" ====================================================");
+			System.out.println(" !       ------ Ung dung ban ve may bay -----       !");
+			System.out.println(" !             1. Quan ly ve.                       !");
+			System.out.println(" !             2. Quan ly khach hang.               !");
+			System.out.println(" !             3. Quan ly chuyen bay.               !");
+			System.out.println(" !             4. Quan ly san bay.                  !");
+			System.out.println(" !             5. Quan ly may bay.                  !");
+			System.out.println(" !             6. Quan ly nhan vien.                !");
+			System.out.println(" !             7. Quan ly hoa don.                  !");
+			System.out.println(" !             8. Quan ly chi tiet hoa don.         !");
+			System.out.println(" !             0. Thoat.                            !");
+			System.out.println(" ====================================================");			
 			Scanner sc = new Scanner(System.in);
+			System.out.print(" - Chon hanh dong: ");
 			int action = sc.nextInt();
+			System.out.println("");
 			switch(action) {
-			case 1:
+			case 6:
 				QuanLyDSNV nv = new QuanLyDSNV();
 				nv.menu();
 				break;
@@ -24,25 +39,29 @@ public class Main {
 				QuanLyDSKH kh = new QuanLyDSKH();
 				kh.menu();
 				break;
-			case 3:
+			case 4:
 				QuanLyDSSB sb = new QuanLyDSSB();
 				sb.menu();
 				break;
-			case 4: 
+			case 7: 
 				QuanLyDSHD hd = new QuanLyDSHD();
 				hd.menu();
 				break;
-			case 5: 
+			case 3: 
 				QuanLyDSCB cb = new QuanLyDSCB();
 				cb.menu();
 				break;
-			case 6: 
+			case 1: 
 				DanhSachVe dsVe = new DanhSachVe();
-		        dsVe.NhapDanhSach();
-		        dsVe.DocFileJava("Ve.txt");
-		        dsVe.XuatDanhSach();
 		        dsVe.ThaoTac();
-
+				break;
+			case 8:
+				QuanLyDSCTHD cthd = new QuanLyDSCTHD();
+				cthd.menu();
+				break;
+			case 5: 
+				QuanLyDSMB mb = new QuanLyDSMB();
+				mb.menu();
 				break;
 			default :
 				return;
